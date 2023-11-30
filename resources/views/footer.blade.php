8f8bf7e88e505c6392f3cbf1bfd19d28b4a4b335
Fajhri Ramadhan A. <115614228+fajhriramadhan@users.noreply.github.com>
2023-11-30 15:18:57 +0700

    pertemuan 30 nov

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,6 @@
+<br/>
+<br/>
+<hr/>
+<footer>
+    <p>&copy; Fajhri Ramadhan A. 2022</p>
+</footer>
--- resources/views/tambah.blade.php
+++ resources/views/tambah.blade.php
@@ -13,10 +13,24 @@
 	<br/>
 	<br/>
 
-	<form action="/pegawai/store" method="post">
+	<form action="/pegawai/store" method="post" class="form-horizontal">
 		{{ csrf_field() }}
-		Nama <input type="text" name="nama"> <br/>
-		Jabatan <input type="text" name="jabatan"> <br/>
+        <div class="form-group row">
+            <label for="nama" class="col-xs-4 col-form-label mr-2">Nama</label>
+            <div class="col-xs-8">
+                <input type="text" class="form-control" id="nama">
+
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="jabatan" class="col-xs-4 col-form-label mr-2">Jabatan</label>
+            <div class="col-xs-8">
+                <input type="text" class="form-control" id="jabatan">
+
+            </div>
+        </div>
+		{{-- Nama <input type="text" name="nama"> <br/>
+		Jabatan <input type="text" name="jabatan"> <br/> --}}
 		Umur <input type="number" name="umur"> <br/>
 		Alamat <textarea name="alamat"></textarea> <br/>
 		<input type="submit" value="Simpan Data">
